5dbdee352d048201f1e5a2c4bfe6969736445c09
setsuna <jonathasoficial1515@gmail.com>
2026-03-02 17:13:23 -0300

    Featch:adiciona istilização

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -6,4 +6,21 @@ class Venda < ApplicationRecord
   accepts_nested_attributes_for :itens_venda, allow_destroy: true, reject_if: :all_blank
 
   validates :itens_venda, presence: true
+
+  before_save :calcular_total
+
+  def recalcular_total
+    calcular_total
+    save
+  end
+
+  private
+
+  def calcular_total
+    self.total = itens_venda.sum do |item|
+      quantidade = item.quantidade || 0
+      preco = item.preco_unitario.presence || item.produto&.preco || 0
+      quantidade * preco
+    end
+  end
 end
